e08d75910c87beead25c6f672dd5118847aa915e
Andres Felipe Vasquez Saldarriaga <avasquez@todo1.com>
2020-09-22 18:40:33 -0500

    i solved the issue that no let me continue the  application correct

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -1,15 +1,15 @@
 //
-//  AppDelegate+notification.m
+//  appDelegate+notification.m
 //  Personas
 //
 //  Created by Andres Felipe Vasquez Saldarriaga on 22/09/20.
 //
 
-#import "AppDelegate+notification.h"
+#import "appDelegate+notification.h"
 //#import "PushPlugin.h"
 #import <objc/runtime.h>
-#import <didm_auth_sdk_iOS/didm_auth_sdk_iOS.h>
 
+#define kApplicationInBackgroundKey @"applicationInBackground"
 
 @implementation AppDelegate (notification)
 
@@ -20,49 +20,13 @@
     method_exchangeImplementations(original, todo1);
 }
 
-
 - (BOOL)application:(UIApplication *)application todo1DidFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-       center.delegate = self;
-       [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error){
-       if(!error){
-       [[UIApplication sharedApplication] registerForRemoteNotifications]; }
-       }];
-       
-       
-       [center setNotificationCategories:[[DetectID sdk] getUNNotificationActionCategoriesForNotifications]];
+    [self application:application todo1DidFinishLaunchingWithOptions:launchOptions];
+
+    
     
     return YES;
-}
-
--(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
-[[DetectID sdk] subscribePayload:notification withCompletionHandler:completionHandler];
-}
--(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler{
-[[DetectID sdk] handleActionWithIdentifier:response];
-}
-
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    [[DetectID sdk] receivePushServiceId:deviceToken];
-}
-
-- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error{ //management error
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler{
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[DetectID sdk]subscribePayload:userInfo];
-    });
-}
-
-
-- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void (^)(void))completionHandler{
-[[DetectID sdk] handleActionWithIdentifier:identifier forNotification:userInfo];
-completionHandler();
-    
 }
 
 @end
